a91241408cee7a149743fe441ef78ad36eea7215
bitchikogulara <bichiko2004@gmail.com>
2025-03-09 20:50:54 +0100

    Added Icon to week 4 project

---
+++ week_4/week_4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  week_4
+//
+//  Created by mac on 09.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week_4/week_4/week_4App.swift
@@ -0,0 +1,17 @@
+//
+//  week_4App.swift
+//  week_4
+//
+//  Created by mac on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct week_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
