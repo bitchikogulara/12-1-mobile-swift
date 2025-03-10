a66956e8fbca407bbb32764574dfacfe80032888
bitchikogulara <bichiko2004@gmail.com>
2025-03-10 17:33:19 +0100

    Added week_6 project directory

---
+++ week_6/week_6/Task_1.swift
@@ -0,0 +1,20 @@
+//
+//  Task_1.swift
+//  week_6
+//
+//  Created by mac on 10.03.25.
+//
+
+import SwiftUI
+
+struct Task_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Task_1_Previews: PreviewProvider {
+    static var previews: some View {
+        Task_1()
+    }
+}
---
+++ week_6/week_6/week_6App.swift
@@ -0,0 +1,17 @@
+//
+//  week_6App.swift
+//  week_6
+//
+//  Created by mac on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct week_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
